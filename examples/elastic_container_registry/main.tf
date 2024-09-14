30a2fb3589c7955eeed85b1eb9a221deba9ab022
Benjamin Clark <bclark@sudoblark.com>
2024-09-14 10:28:41 +0100

    Add examples and update CI pipeline

---
+++ examples/elastic_container_registry/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.63.0"
+    }
+  }
+  required_version = "~> 1.5.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "elastic_container_registry" {
+  source = "github.com/sudoblark/sudoblark.terraform.module.aws.elastic_container_registry?ref=1.0.0"
+
+  application_name   = var.application_name
+  environment        = var.environment
+  raw_ecr_registries = local.raw_ecr_registries
+
+}
